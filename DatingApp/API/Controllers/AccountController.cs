b0f7b146458585584230181168c30ae18c2c991a
RahulNaval21 <rahulnaval157821@gmail.com>
2024-08-05 21:09:00 +0530

    add login endpoint

--- DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Text;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
+using API.DTOs;
 
 namespace API.Controllers;
 
@@ -25,6 +26,26 @@ public class AccountController(DataContext context) : BaseApiController
 
         context.Users.Add(user);
         await context.SaveChangesAsync();
+
+        return user;
+    }
+
+    [HttpPost("login")]
+
+    public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(x=> 
+        x.UserName == loginDTO.Username.ToLower());
+
+        if(user == null) return Unauthorized("Invalid Username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+        for(int i=0; i<computedHash.Length; i++){
+            if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+        }
 
         return user;
     }
