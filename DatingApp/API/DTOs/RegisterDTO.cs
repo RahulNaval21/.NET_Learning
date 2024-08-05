4b9725e6c517f29a2776fb0b7c83b52a3c7aa423
RahulNaval21 <rahulnaval157821@gmail.com>
2024-08-05 16:54:27 +0530

    Using DTO

---
+++ DatingApp/API/DTOs/RegisterDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API;
+
+public class RegisterDTO
+{
+    [Required]
+    [MinLength(2)]
+    public required string Username {get;set;}
+
+    [Required]
+    public required string Password {get;set;}
+}
