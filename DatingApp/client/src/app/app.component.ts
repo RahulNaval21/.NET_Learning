d1403d1001ea1b14790ce1c911f7195f635bbb04
RahulNaval21 <rahulnaval157821@gmail.com>
2024-08-19 11:12:21 +0530

    parent to child communication

--- DatingApp/client/src/app/app.component.ts
+++ DatingApp/client/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
@@ -14,13 +13,11 @@ import { HomeComponent } from "./home/home.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
-  http = inject(HttpClient);
+  
   private accountService = inject(AccountService);
-  title = 'client';
-  users: any;
+ 
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -31,11 +28,5 @@ export class AppComponent implements OnInit{
     this.accountService.currentUser.set(user);
   }
 
-  getUsers(){
-    this.http.get('http://localhost:5140/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('req succ completed',this.users)
-    })
-  }
+  
 }
