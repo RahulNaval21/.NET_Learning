25627c9b78fc3df6e3324a005903f72a490447c3
RahulNaval21 <rahulnaval157821@gmail.com>
2024-08-19 15:18:56 +0530

    add routes

--- DatingApp/client/src/app/register/register.component.ts
+++ DatingApp/client/src/app/register/register.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, input, output } from '@angular/core';
+import { Component, EventEmitter, inject,  output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -9,16 +11,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  usersFromHomeComponent = input.required<any>()
-   cancelRegister = output<boolean>();
+  private accountService = inject(AccountService);
+  private toastr = inject(ToastrService)
+  cancelRegister = output<boolean>();
 
   model: any = {}
-
-  register(){
-    console.log(this.model);
-  }
 
   cancel(){
     this.cancelRegister.emit(false);
   }
+  register(){
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => this.toastr.error(error.error)
+    });
+
+}
 }
