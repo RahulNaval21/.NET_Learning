aec570ed1ef1e1331a81c4452a55640289d79cb6
RahulNaval21 <rahulnaval157821@gmail.com>
2024-08-21 15:35:00 +0530

    Exception Handling Middleware

--- DatingApp/API/Controllers/BuggyController.cs
+++ DatingApp/API/Controllers/BuggyController.cs
@@ -18,15 +18,18 @@ public class BuggyController(DataContext context) : BaseApiController
     public ActionResult<AppUser> GetNotFound()
     {
         var thing = context.Users.Find(-1);
-        if(thing==null) return NotFound();
+        if (thing == null) return NotFound();
         return thing;
     }
     [HttpGet("server-error")]
     public ActionResult<AppUser> GetServerError()
     {
+
         var thing = context.Users.Find(-1) ?? throw new Exception("A bad thing has happened");
-        
+
         return thing;
+
+
     }
     [HttpGet("bad-request")]
     public ActionResult<string> GetBadRequest()
--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","http://localhost:4200"));
 
 app.UseAuthentication();
