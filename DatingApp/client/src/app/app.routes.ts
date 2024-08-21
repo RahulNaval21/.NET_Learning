0a180490d1a7fe95a0c591708ed600890e70419b
RahulNaval21 <rahulnaval157821@gmail.com>
2024-08-21 17:47:51 +0530

    add a server error page

--- DatingApp/client/src/app/app.routes.ts
+++ DatingApp/client/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -21,8 +22,9 @@ export const routes: Routes = [
             {path: 'messages', component: MessagesComponent},
             ]
     },
-    {path: 'not-found', component: NotFoundComponent},
     {path: 'errors', component: TestErrorsComponent},
+    {path: 'not-found', component: NotFoundComponent},
+    {path: 'server-error', component: ServerErrorComponent},
     {path: '**', component: HomeComponent, pathMatch: 'full'}
 ];
  
