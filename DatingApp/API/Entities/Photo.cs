5892fe41987fe10e8cc8c4a7563b8640143ea7db
RahulNaval21 <rahulnaval157821@gmail.com>
2024-08-22 02:57:29 +0530

    EF relationships

--- DatingApp/API/Entities/Photo.cs
+++ DatingApp/API/Entities/Photo.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities;
 
+[Table("Photos")]
 public class Photo
 {
     public int Id { get; set; }
@@ -9,4 +12,9 @@ public class Photo
     public bool IsMain { get; set; }
 
     public string? PublicId { get; set; }
+
+    // Navigation Property
+    public int AppUserId { get; set; }
+
+    public AppUser AppUser { get; set; } = null!;
 }
