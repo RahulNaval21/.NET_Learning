66af880b3d6499d69d1b564d52c699247e1af8e7
RahulNaval21 <rahulnaval157821@gmail.com>
2024-08-23 15:22:17 +0530

    Create A Repository Interface

---
+++ DatingApp/API/interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using API.Entities;
+
+namespace API.interfaces;
+
+public interface IUserRepository
+{
+    void Update(AppUser user);
+    Task<bool> SaveAllAsync();
+
+    Task<IEnumerable<AppUser>> GetUsersAsync();
+    Task<AppUser> GetUserByIdAsync(int id);
+    Task<AppUser> GetUserByUsernameAsync(string username);
+
+    
+
+}
