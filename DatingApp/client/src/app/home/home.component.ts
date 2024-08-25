914671f792ea2f97936996fb68e4815373d954fb
RAHUL <rahulnaval157821@gmail.com>
2024-08-25 16:44:36 +0530

    add a member service and retrive the list of members

--- DatingApp/client/src/app/home/home.component.ts
+++ DatingApp/client/src/app/home/home.component.ts
@@ -9,14 +9,8 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
-  http = inject(HttpClient);
+export class HomeComponent {
   registerMode = false;
-  users: any;
-
-  ngOnInit(): void {
-    this.getUsers();
-  }
 
   registerToggle(){
     this.registerMode = !this.registerMode;
@@ -26,11 +20,4 @@ export class HomeComponent implements OnInit{
     this.registerMode = event;
   }
 
-  getUsers(){
-    this.http.get('http://localhost:5140/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('req succ completed',this.users)
-    })
-  }
 }
